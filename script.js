c7bf8db0a47bf5dcdad2cf7f54ea80d0c019b546
GPC Thushani <charithathushani2003@gmail.com>
2025-08-21 20:51:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-// Smooth scroll for all anchor links
+// Smooth scroll
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function(e) {
     e.preventDefault();
     const target = document.querySelector(this.getAttribute('href'));
     if(target){
-      target.scrollIntoView({
-        behavior: 'smooth'
-      });
+      target.scrollIntoView({ behavior: 'smooth' });
     }
   });
 });
